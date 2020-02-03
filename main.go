81b06501a682429669fd2f9c63fef8fb9b357696
vroyibg <vroyibg@gmail.com>
2020-02-03 08:18:25 +0700

    login api

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
+	"log"
 )
 import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
@@ -23,20 +24,52 @@ func main() {
 	r.POST("/user", func(context *gin.Context) {
 		PostUser(context)
 	})
+
+	r.POST("/session", func(context *gin.Context) {
+		Login(context);
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	defer db.Close()
+}
+
+func Login(context *gin.Context) {
+	var loginModel UserLoginModel
+	if context.BindJSON(&loginModel) == nil {
+		rows, err := db.Query("select * from users where username='"+loginModel.UserName+"' limit 1")
+		if err != nil {
+			context.JSON(400, Error{Message: err.Error()})
+		} else {
+			var user User
+			if rows.Next() {
+				if err := rows.Scan(&user.Id, &user.UserName, &user.PasswordHash); err != nil {
+					log.Println(err.Error())
+					context.JSON(500, Error{Message:"Server error"})
+				} else {
+					if CheckPasswordHash(loginModel.Password,user.PasswordHash   ) {
+						log.Println("User "+user.UserName+" logged in")
+						context.JSON(200,user )
+					} else {
+						context.JSON(400, Error{Message:"Incorrect password"})
+					}
+				}
+			} else {
+				context.JSON(404, Error{Message: "User name or password incorrect."})
+			}
+		}
+	} else {
+		context.JSON(400, Error{Message:"Bad request"})
+	}
 }
 
 func PostUser(context *gin.Context) {
 	var user UserCreateModel
 	if context.BindJSON(&user) == nil {
-		fmt.Println(user.Password)
-		fmt.Println(user.UserName)
 		passwordHash,_ := HashPassword(user.Password)
 		_,err := db.Exec("insert into users (UserName, PasswordHash) values('"+user.UserName+"',"+"'"+passwordHash+"')");
 		if err != nil {
 			context.JSON(400, Error {Message: err.Error()})
 		} else {
+			fmt.Println("User " + user.UserName +" registered")
 			context.JSON(200,user);
 		}
 	}
@@ -70,6 +103,12 @@ type UserCreateModel struct {
 type UserLoginModel struct {
 	UserName string
 	Password string
+}
+
+type User struct {
+	Id int
+	UserName string
+	PasswordHash string
 }
 
 type Error struct {
